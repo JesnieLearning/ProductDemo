e43a38eb97aef33a5a04bf6e585cb9386924daf0
unknown <jesnielearning0423@hotmail.com>
2021-05-18 10:39:06 +0800

    Check in product demo solution

---
+++ ProductDemoAPI/Data/DemoDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ProductDemoAPI.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductDemoAPI.Data
+{
+    public class DemoDBContext : DbContext
+    {
+        private readonly IConfiguration _config;
+        public  DemoDBContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder bldr)
+        {
+            base.OnConfiguring(bldr);
+
+            bldr.UseSqlServer(_config.GetConnectionString("DemoDBAppCon"));
+        }
+    }
+}
